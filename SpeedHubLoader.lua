53cea4ffb210d1e9b9b144b2a7e99cb5bf45bf51
muzammil909900 <143308858+muzammil909900@users.noreply.github.com>
2025-06-30 11:46:18 +0500

    Update SpeedHubLoader.lua

--- SpeedHubLoader.lua
+++ SpeedHubLoader.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/muzammil909900/SpeedHubX/refs/heads/main/SpeedHubLua.txt"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/muzammil909900/UpdatedScript/refs/heads/main/86b6d6f92d02479a59d8f680480b5ad7.txt"))()
